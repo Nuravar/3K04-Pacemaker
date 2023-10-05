c7e435b3c4678c4bf3d198ef2f66cc7157ad33af
Varun Kothandaraman <91991164+Nuravar@users.noreply.github.com>
2023-10-05 10:51:48 -0400

    fixed up the code

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -1,81 +1,95 @@
-# GUI
-import customtkinter as ctk
-import tkinter as tk
+import customtkinter
 
+class ParametersWindow(customtkinter.CTkToplevel):
+        def __init__(self, *args, **kwargs):
+            super().__init__(*args, **kwargs)
+            self.geometry("400x300")
+            self.label = customtkinter.CTkLabel(self, text="ToplevelWindow")
+            self.label.pack(padx=20, pady=20)
 
-class MainApp:
-    def __init__(self, root):
-        self.root = root
-        self.root.title('Pacemaker')
-        self.root.geometry('800x600')
-
+class App(customtkinter.CTk):
+    def __init__(self): #initializes, for all tkinter code, you find replace app with self
+        super().__init__()
+        self.title('Pacemaker')
+        self.geometry('800x600')
+        # self.custom_font = customtkinter.CTkFont(family="Calibri", size=14, weight='bold')
         self.create_welcome_screen()
+        
 
     def create_welcome_screen(self):
-        self.welcome_frame = ctk.CTkFrame(self.root)
+        self.welcome_frame = customtkinter.CTkFrame(self)
         self.welcome_frame.pack(fill='both', expand=True)
 
-        ctk.CTkLabel(self.welcome_frame, text='Welcome!').pack(pady=20)
-        ctk.CTkButton(self.welcome_frame, text='Login', command=self.show_login_screen).pack(pady=10)
-        ctk.CTkButton(self.welcome_frame, text='Create an Account', command=self.show_create_account_screen).pack(pady=10)
-        ctk.CTkButton(self.welcome_frame, text='Continue as Guest', command=self.show_main_screen).pack(pady=10)
+        customtkinter.CTkLabel(self.welcome_frame, text='Welcome!').pack(pady=20)
+        customtkinter.CTkButton(self.welcome_frame, text='Login', command=self.show_login_screen).pack(pady=10)
+        customtkinter.CTkButton(self.welcome_frame, text='Create an Account', command=self.show_create_account_screen).pack(pady=10)
+        customtkinter.CTkButton(self.welcome_frame, text='Continue as Guest', command=self.show_main_screen).pack(pady=10)
+
 
     def show_login_screen(self):
         self.welcome_frame.pack_forget()
-        self.login_frame = ctk.CTkFrame(self.root)
+        self.login_frame = customtkinter.CTkFrame(self)
         self.login_frame.pack(fill='both', expand=True)
 
-        ctk.CTkLabel(self.login_frame, text='Login Screen').pack(pady=20)
-        ctk.CTkButton(self.login_frame, text='Back', command=self.back_to_welcome).pack(pady=10)
+        customtkinter.CTkLabel(self.login_frame, text='Login Screen').pack(pady=20)
+        customtkinter.CTkButton(self.login_frame, text='Back', command=self.back_to_welcome).pack(pady=10)
 
     def show_create_account_screen(self):
         self.welcome_frame.pack_forget()
-        self.create_account_frame = ctk.CTkFrame(self.root)
+        self.create_account_frame = customtkinter.CTkFrame(self)
         self.create_account_frame.pack(fill='both', expand=True)
 
-        ctk.CTkLabel(self.create_account_frame, text='Create Account Screen').pack(pady=20)
-        ctk.CTkButton(self.create_account_frame, text='Back', command=self.back_to_welcome).pack(pady=10)
+        customtkinter.CTkLabel(self.create_account_frame, text='Create Account Screen').pack(pady=20)
+        customtkinter.CTkButton(self.create_account_frame, text='Back', command=self.back_to_welcome).pack(pady=10)
+
 
     def back_to_welcome(self):
-        for widget in self.root.winfo_children():
+        for widget in self.winfo_children():
             widget.pack_forget()
         self.create_welcome_screen()
 
     def show_main_screen(self):
-        for widget in self.root.winfo_children():
+        for widget in self.winfo_children():
             widget.pack_forget()
 
-        self.main_frame = ctk.CTkFrame(self.root)
+        self.main_frame = customtkinter.CTkFrame(self)  
         self.main_frame.pack(fill='both', expand=True)
 
-        self.nav_bar = ctk.CTkFrame(self.main_frame)
+        self.nav_bar = customtkinter.CTkFrame(self.main_frame)
         self.nav_bar.pack(fill='x')
-        ctk.CTkButton(self.nav_bar, text='Sign Out', command=self.back_to_welcome).pack(side='right')
-        ctk.CTkButton(self.nav_bar, text='Parameters', command=self.show_parameters_popup).pack(side='right')
+        customtkinter.CTkButton(self.nav_bar, text='Sign Out', command=self.back_to_welcome, fg_color="#1d3557").pack(side='right')
+        customtkinter.CTkButton(self.nav_bar, text='⚙ Options', command=self.show_parameters_popup, fg_color="#1d3557").pack(side='right')
+        self.toplevel_window = None
 
-        self.body_frame = ctk.CTkFrame(self.main_frame)
+        self.switch_var = customtkinter.StringVar(value="on")
+        self.switch = customtkinter.CTkSwitch(self.nav_bar, text="🌙", command=self.theme_event, variable=self.switch_var, onvalue="on", offvalue="off")
+        self.switch.pack(side="left")
+        
+        self.body_frame = customtkinter.CTkScrollableFrame(self.main_frame)
         self.body_frame.pack(fill='both', expand=True, pady=20)
-        ctk.CTkLabel(self.body_frame, text='Main Body Content').pack()
+        customtkinter.CTkLabel(self.body_frame, text='Main Body Content').pack()
 
-        self.footer_frame = ctk.CTkFrame(self.main_frame)
+        self.footer_frame = customtkinter.CTkFrame(self.main_frame)
         self.footer_frame.pack(fill='x', side='bottom')
-        ctk.CTkLabel(self.footer_frame, text='Footer Content').pack()
+        customtkinter.CTkLabel(self.footer_frame, text='Footer Content').pack()
 
         self.show_parameters_popup()
 
+
+    def theme_event(self):
+        print("switch toggled, current value:", self.switch_var.get())
+        if self.switch_var.get() == "on":
+            customtkinter.set_appearance_mode("dark")
+            self.switch.configure(text="🌙")
+        else: 
+            customtkinter.set_appearance_mode("light")
+            self.switch.configure(text="🌞")       
+
     def show_parameters_popup(self):
-        self.parameters_popup = tk.Toplevel(self.root)
-        self.parameters_popup.title('Parameters')
-        ctk.CTkLabel(self.parameters_popup, text='Enter Frequency:').pack(pady=10)
-        self.frequency_entry = ctk.CTkEntry(self.parameters_popup)
-        self.frequency_entry.pack(pady=10)
-
-        ctk.CTkLabel(self.parameters_popup, text='Enter Name:').pack(pady=10)
-        self.name_entry = ctk.CTkEntry(self.parameters_popup)
-        self.name_entry.pack(pady=10)
-
-        ctk.CTkButton(self.parameters_popup, text='Save', command=self.save_parameters).pack(pady=10)
-        ctk.CTkButton(self.parameters_popup, text='Close', command=self.parameters_popup.destroy).pack(pady=10)
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = ParametersWindow(self)  # create window if its None or destroyed
+        else:
+            self.toplevel_window.focus()  # if window exists focus it
 
     def save_parameters(self):
         self.frequency = self.frequency_entry.get()
@@ -85,6 +99,5 @@ class MainApp:
 
 
 if __name__ == '__main__':
-    root = tk.Tk()
-    app = MainApp(root)
-    root.mainloop()
+    app = App()
+    app.mainloop()  
